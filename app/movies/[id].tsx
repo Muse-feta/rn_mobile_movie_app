c24bd9fd9319e68751d8aaee6a0788017dbdb8a7
Muse-feta <fetamuse@gmail.com>
2025-04-10 23:41:23 +0300

    first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams, useSearchParams } from 'expo-router/build/hooks';
+
+const MovieDetails = () => {
+    // const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>MovieDetails of</Text>
+    </View>
+  );
+}
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({})
