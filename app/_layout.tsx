731798427952872a127652710e316134ed5977bf
Muse-feta <fetamuse@gmail.com>
2025-04-11 13:11:43 +0300

    finished the status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,27 @@
 import { Stack } from "expo-router";
-import './global.css';
+import { StatusBar } from "react-native";
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen 
-    name="(tabs)" 
-    options={{ headerShown: false }} 
-    />
-    <Stack.Screen 
-    name="movies/[id]" 
-    options={{ headerShown: false }} 
-    />
-  </Stack>;
+  return (
+    <>
+      {/* Make the status bar transparent */}
+      {/* <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      /> */}
+      {/* <StatusBar backgroundColor="black" barStyle="light-content" /> */}
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+// import { useEffect } from "react";
+// import { View, Text } from "react-native";
+// import { router } from "expo-router";
+
+// export default function SplashScreen() {
+//   useEffect(() => {
+//     setTimeout(() => {
+//       // Navigate to main tab after 2 seconds
+//       router.replace("/(tabs)");
+//     }, 5000);
+//   }, []);
+
+//   return (
+//     <View className="flex-1 items-center justify-center bg-white">
+//       <Text className="text-xl font-bold">🎬 Movie App</Text>
+//     </View>
+//   );
+// }
