a978cc30167e6a8d706415fdec27503eb74f4425
Muse-feta <fetamuse@gmail.com>
2025-04-12 20:00:02 +0300

    minor change on serach bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
 
@@ -6,10 +6,14 @@ const SearchBar = () => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
+      <TextInput onPress={() => {}} placeholder='Search'
+      value='' 
+      onChangeText={() => {}}
+      placeholderTextColor='#ab8bff'
+      className='flex m1-2 px-4'
+      />
     </View>
   )
 }
 
 export default SearchBar
-
-const styles = StyleSheet.create({})
