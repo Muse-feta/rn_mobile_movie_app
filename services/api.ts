ec73257e921c282d7faa2c798c2394fd7fc511b0
Muse-feta <fetamuse@gmail.com>
2025-04-26 18:56:59 +0300

    finished the movie detail screen

--- services/api.ts
+++ services/api.ts
@@ -19,3 +19,21 @@ export const fetchMovies = async ({query}: {query: string}) => {
   const data = await res.json();
   return data.results;
 }
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+  try {
+    const res = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+      method: "GET",
+      headers: TMDB_CONFIG.Headers,
+    })
+
+    if(!res.ok) {
+      throw new Error("Failed to fetch movie details", {cause: res.status});
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
